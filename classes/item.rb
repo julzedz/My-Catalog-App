99239b2b325273764839f5be7ee43fd0f9a436a2
julzedz <edoziechukwuma@gmail.com>
2023-07-09 07:23:52 +0100

    Define Item class

---
+++ classes/item.rb
@@ -0,0 +1,22 @@
+require 'date'
+
+class Item
+  attr_accessor :archived, :genre, :author, :label
+
+  def initialize(genre, author, label, publish_date, archived: false)
+    @id = Random.rand(1..1000)
+    @genre = genre
+    @author = author
+    @label = label
+    @publish_date = Date.parse(publish_date)
+    @archived = archived
+  end
+
+  def can_be_archived?
+    Date.today - @publish_date > (10 * 365)
+  end
+
+  def move_to_archive?
+    self.archived = can_be_archived?
+  end
+end
