367dd6291c49d8b4af88693a570501119ddad696
julzedz <edoziechukwuma@gmail.com>
2023-07-10 08:32:56 +0100

    Create app.rb file

---
+++ app.rb
@@ -0,0 +1,44 @@
+class App
+  # def initialize end
+
+  def list_all_books
+    puts 'LISTING ALL BOOKS'
+  end
+
+  def list_all_music_albums
+    puts 'LISTING ALL MUSIC ALBUMS'
+  end
+
+  def list_all_games
+    puts 'LISTING ALL GAMES'
+  end
+
+  def list_all_genres
+    puts 'LISTING ALL GENRES'
+  end
+
+  def list_all_labels
+    puts 'LISTING ALL LABELS'
+  end
+
+  def list_all_authors
+    puts 'LISTING ALL AUTHORS'
+  end
+
+  def add_book
+    puts 'ADDING A NEW BOOK'
+  end
+
+  def add_music_album
+    puts 'ADDING A NEW MUSIC ALBUM'
+  end
+
+  def add_game
+    puts 'ADDING A NEW GAME'
+  end
+
+  def quit
+    puts 'QUITTING APP...'
+    exit
+  end
+end
