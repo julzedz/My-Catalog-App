3f06046642eae4433941f21045f5a90eb5d1bc9b
julzedz <edoziechukwuma@gmail.com>
2023-07-11 09:26:44 +0100

    Add label class

--- label.rb
+++ label.rb
@@ -1,5 +1,19 @@
+require_relative 'item'
+
 class Label
-  def initialize
-    
+  attr_accessor :id, :title, :color, :items
+
+  def initialize(title, color)
+    @id = Random.rand(1...1000)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    return unless item.is_a?(Item) && !@item.include?(item)
+
+    item.label = self
+    @items << item
   end
 end
